d6901fd710b0ab62b05078b2a68dff8dbee2f284
dilip0418 <sudheer0418@gmail.com>
2023-07-12 14:33:48 +0530

    first commit

--- JavaFundamentals/src/com/dilip/dayOneCodes/HelloWorld.java
+++ JavaFundamentals/src/com/dilip/dayOneCodes/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello This is my first commit to git!");
+		System.out.println("Hello This is my First commit to git!");
 		
 	}
 

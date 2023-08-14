8846812394d3f8b62c47ef7a7b3d01953bc06168
dilip0418 <sudheer0418@gmail.com>
2023-08-14 23:40:28 +0530

    comparable interface commit 1

---
+++ JavaFundamentals/src/com/dilip/dayTenCodes/comparableDemo/ComparableExample.java
@@ -0,0 +1,43 @@
+package com.dilip.dayTenCodes.comparableDemo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Person implements Comparable<Person> {
+	private String name;
+	private int age;
+
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	@Override
+	public int compareTo(Person otherPerson) {
+		return Integer.compare(this.age, otherPerson.age);
+	}
+}
+
+public class ComparableExample {
+	public static void main(String[] args) {
+		List<Person> people = new ArrayList<>();
+		people.add(new Person("Alice", 25));
+		people.add(new Person("Bob", 30));
+		people.add(new Person("Charlie", 22));
+
+		Collections.sort(people);
+
+		for (Person person : people) {
+			System.out.println(person.getName() + " - " + person.getAge());
+		}
+	}
+}

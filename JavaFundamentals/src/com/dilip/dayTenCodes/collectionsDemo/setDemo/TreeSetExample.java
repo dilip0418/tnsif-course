71643939731870755dc40dc7be3003357af81412
dilip0418 <sudheer0418@gmail.com>
2023-08-18 10:45:01 +0530

    TreeSet modified commit

--- JavaFundamentals/src/com/dilip/dayTenCodes/collectionsDemo/setDemo/TreeSetExample.java
+++ JavaFundamentals/src/com/dilip/dayTenCodes/collectionsDemo/setDemo/TreeSetExample.java
@@ -54,25 +54,24 @@ public class TreeSetExample {
 		System.out.println(set.floor(5)); // return 4 because condition ( 4 is the greatest of all the elements that are
 											// less then 5)
 
-		
-		
-		/*
-		 * 
-		 * // Set<Student> students = new TreeSet<>(Student.idComparator); //
-		 * Set<Student> students = new TreeSet<>(Student.nameComparator); //
-		 * Set<Student> students = new TreeSet<>(Student.ageComparator); Set<Student>
-		 * students = new TreeSet<>(Student.nameAgeComparator);
-		 * 
-		 * students.add(new Student(101,"Gani",23)); students.add(new
-		 * Student(102,"Shivesh",23)); students.add(new Student(103,"Rohith",22));
-		 * students.add(new Student(104,"Ravi",25)); students.add(new
-		 * Student(105,"Ravi",21)); students.add(new Student(106,"Mahesh",24));
-		 * 
-		 * // System.out.println(students);
-		 * 
-		 * Iterator<Student> it = students.iterator(); while(it.hasNext()) {
-		 * System.out.println(it.next()); }
-		 */
+		Set<Student> students = new TreeSet<>(Student.idComparator);
+		// Set<Student> students = new TreeSet<>(Student.nameComparator);
+		// Set<Student> students = new TreeSet<>(Student.ageComparator);
+		// Set<Student> students = new TreeSet<>(Student.nameAgeComparator);
+
+		students.add(new Student(101, "Gani", 23));
+		students.add(new Student(102, "Shivesh", 23));
+		students.add(new Student(103, "Rohith", 22));
+		students.add(new Student(104, "Ravi", 25));
+		students.add(new Student(105, "Ravi", 21));
+		students.add(new Student(106, "Mahesh", 24));
+
+		// System.out.println(students);
+
+		Iterator<Student> it1 = students.iterator();
+		while (it1.hasNext()) {
+			System.out.println(it1.next());
+		}
 	}
 
 }
